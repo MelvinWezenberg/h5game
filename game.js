bb89fa6fdb364665e1636410a565f462cdc30fba
MelvinWezenberg <33494056+MelvinWezenberg@users.noreply.github.com>
2024-03-13 15:23:16 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 class Game {
+  class Game {
   constructor() {
+    // Define variables
     this.score = 0;
     this.choice = '';
     this.muted = false;
@@ -11,8 +13,8 @@ class Game {
     this.playButton = document.getElementById('playButton');
     this.headsButton = document.getElementById('headsButton');
     this.tailsButton = document.getElementById('tailsButton');
-    this.muteButton = document.getElementById('muteButton');
 
+    // On click listeners for the game's buttons.
     this.playButton.addEventListener('click', () => {
       this.erase();
       this.play();
@@ -28,39 +30,31 @@ class Game {
       this.flipCoin();
     });
 
-    this.muteButton.addEventListener('click', () => {
-      let soundString = this.muted ? 'on' : 'off';
-      this.muteButton.innerHTML = this.muted ? 'Mute sound' : 'Un-mute sound';
-      this.muted = !this.muted;
-      adConfig({
-        sound: soundString
-      });
-    });
-
-    adConfig({ sound: 'on' }); // Initial sound config
     this.erase();
   }
 
+// Start the game
   play() {
     if (this.shouldShowAdOnPlay) {
       this.shouldShowAdOnPlay = false;
 
       adBreak({
-        type: 'next', 
+        type: 'next',  // ad shows at start of next level
         name: 'restart-game',
-        beforeAd: () => { this.disableButtons(); },
-        afterAd: () => { this.enableButtons(); } 
+        beforeAd: () => { this.disableButtons(); },  // You may also want to mute the game's sound.
+        afterAd: () => { this.enableButtons(); },    // resume the game flow.
       });
     }
 
     this.score = 0;
     this.canvas.fillText('Score: ' + this.score, 8, 26);
     this.canvas.fillText('Heads or Tails?', 66, 150);
-    this.playButton.style.display = 'none';
-    this.headsButton.style.display = 'inline-block';
-    this.tailsButton.style.display = 'inline-block';
+    this.playButton.style.display = 'none'
+    this.headsButton.style.display = 'inline-block'
+    this.tailsButton.style.display = 'inline-block'
   }
 
+  // Flip the coin
   flipCoin() {
     this.headsButton.disabled = true;
     this.tailsButton.disabled = true;
@@ -71,10 +65,16 @@ class Game {
     setTimeout(() => { this.coinLanded() }, 2000);
   }
 
+  // Logic for when the coin lands
   coinLanded() {
     this.headsButton.disabled = false;
     this.tailsButton.disabled = false;
-    let sideUp = (Math.random() < 0.5) ? 'Heads' : 'Tails';
+    let sideUp;
+    if(Math.random() < 0.5) {
+      sideUp = 'Heads';
+    } else {
+      sideUp = 'Tails';
+    }
 
     if (sideUp === this.choice ) {
       this.win(sideUp);
@@ -83,6 +83,7 @@ class Game {
     }
   }
 
+  // Guess the flip correctly
   win(sideUp) {
     this.erase();
     this.score += 1;
@@ -91,18 +92,21 @@ class Game {
     this.canvas.fillText('Guess again', 70, 200);
   }
 
+  // Guess the flip incorrectly
+// Guess the flip incorrectly
   lose(sideUp) {
-    this.erase();
+    this.erase()
     this.canvas.fillText('Sorry, it was ' + sideUp, 50, 100);
     this.canvas.fillText('Your score was ' + this.score, 50, 150);
     this.canvas.fillText('Want to play again?', 45, 200);
 
-    this.playButton.style.display = 'inline-block';
-    this.headsButton.style.display = 'none';
-    this.tailsButton.style.display = 'none';
+    this.playButton.style.display = 'inline-block'
+    this.headsButton.style.display = 'none'
+    this.tailsButton.style.display = 'none'
     this.shouldShowAdOnPlay = true;
   }
 
+  // Erase the canvas
   erase() {
     this.canvas.fillStyle = '#ADD8E6';
     this.canvas.fillRect(0, 0, 300, 300);
